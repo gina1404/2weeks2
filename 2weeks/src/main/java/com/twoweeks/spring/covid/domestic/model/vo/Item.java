c682e844a3de2b0ab1a68d223f76688ee36e7d6e
BurgerBum <fsm15@naver.com>
2021-07-24 01:43:25 +0900

    국내현황 API 변수 VO생성   -인범-

---
+++ 2weeks/src/main/java/com/twoweeks/spring/covid/domestic/model/vo/Item.java
@@ -0,0 +1,22 @@
+package com.twoweeks.spring.covid.domestic.model.vo;
+
+import lombok.Data;
+
+@Data
+public class Item {
+
+	private double accDefRate;
+	private int accExamCnt;
+	private int accExamCompCnt;
+	private int careCnt;
+	private int clearCnt;
+	private String createDt;
+	private int deathCnt;
+	private int decideCnt;
+	private int examCnt;
+	private int resutlNegCnt;
+	private int seq;
+	private int stateDt;
+	private String stateTime;
+	private String updateDt;
+}
