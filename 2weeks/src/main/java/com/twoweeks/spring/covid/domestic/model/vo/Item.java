cd32de99f8d4645e4e863e30838444dfe569d3c2
dgyou <dgyou@DESKTOP-C8N6M1N>
2021-07-26 21:46:43 +0900

    멤버변수 코멘트 작성

--- 2weeks/src/main/java/com/twoweeks/spring/covid/domestic/model/vo/Item.java
+++ 2weeks/src/main/java/com/twoweeks/spring/covid/domestic/model/vo/Item.java
@@ -5,18 +5,18 @@ import lombok.Data;
 @Data
 public class Item {
 
-	private double accDefRate;
-	private int accExamCnt;
-	private int accExamCompCnt;
-	private int careCnt;
-	private int clearCnt;
-	private String createDt;
-	private int deathCnt;
-	private int decideCnt;
-	private int examCnt;
-	private int resutlNegCnt;
-	private int seq;
-	private int stateDt;
-	private String stateTime;
-	private String updateDt;
+	private double accDefRate; //누적 확진률
+	private int accExamCnt; //누적 검사 수
+	private int accExamCompCnt; // 누적 검사 완료 수
+	private int careCnt; //치료중 환자 수
+	private int clearCnt; //격리해제 수
+	private String createDt; //등록일시분초
+	private int deathCnt; //사망자 수
+	private int decideCnt; //확진자 수
+	private int examCnt; //검사진행 수
+	private int resutlNegCnt; //결과 음성 수
+	private int seq; // 게시글 번호
+	private int stateDt; //기준일
+	private String stateTime; //기준시간
+	private String updateDt; //수정일시분초
 }
