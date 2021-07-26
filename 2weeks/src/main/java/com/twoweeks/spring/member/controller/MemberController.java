d5285a29dc39a4d382a2d1f58954c99860d3cff7
ookitrighthere <moveookz@gmail.com>
2021-07-26 20:29:44 +0900

    아이디 중복체크 추가

--- 2weeks/src/main/java/com/twoweeks/spring/member/controller/MemberController.java
+++ 2weeks/src/main/java/com/twoweeks/spring/member/controller/MemberController.java
@@ -1,7 +1,13 @@
 package com.twoweeks.spring.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.twoweeks.spring.member.model.service.MemberService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,9 +15,23 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MemberController {
 	
+	@Autowired
+	private MemberService memberService;
+	
 	@RequestMapping("/signup")
 	public String SignUp() {
 		return "/member/signup";
 	}
+	
+	//아이디중복체크
+	@GetMapping("/member/idCheck")
+	@ResponseBody
+	public int idCheck(
+			@RequestParam("userId")String userId) {
+		
+		return memberService.idCheck(userId);
+	}
+	
+	
 
 }
