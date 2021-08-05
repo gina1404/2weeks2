9cd3327202db4350e9ca0e42bd84c0b59bc01ca9
이효형 <@DESKTOP-GCFR32J>
2021-08-05 19:36:37 +0900

    관리자페이지 회원테이블 연결

--- 2weeks/src/main/java/com/twoweeks/spring/admin/model/service/AdminService.java
+++ 2weeks/src/main/java/com/twoweeks/spring/admin/model/service/AdminService.java
@@ -1,9 +1,13 @@
 package com.twoweeks.spring.admin.model.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.twoweeks.spring.admin.model.vo.Notice;
+import com.twoweeks.spring.member.model.vo.Member;
 
 public interface AdminService {
 	int insertNotice(Notice n);
+	List<Member> selectMemberList();
 }
